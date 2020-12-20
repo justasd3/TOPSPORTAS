b42eb4f060e8ba881ff514f292e6f2a3e190cd6d
justasd3 <61118821+justasd3@users.noreply.github.com>
2020-12-20 15:50:10 +0200

    pasibaige ivykiai

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -18,6 +18,38 @@
 
                     </div>
                 </div>
+                <h1 class="pt-2"></h1>
+                <div class="card">
+                    <div class="card-header">Pasibaigę įvykiai</div>
+                    <div class="card-body">
+                        <table class="table">
+                            <thead>
+                            <tr>
+                                <th scope="col">Komanda</th>
+                                <th scope="col">Koeficientas</th>
+                                <th scope="col">Rezultatas</th>
+                                <th scope="col">Komanda</th>
+                                <th scope="col">Koeficientas</th>
+                                <th scope="col">Trukmė</th>
+
+                            </tr>
+                            </thead>
+                            @foreach($ivykiai as $ivyk)
+                                @if($ivyk->status == 0)
+                                <tr hidden>@endif
+                                    <th scope="col"> {{$ivyk->komanda_1}}</th>
+                                    <th scope="col"> {{$ivyk->koeficientas_1}}</th>
+                                    <th scope="col"> {{$ivyk->rezultatas}}</th>
+                                    <th scope="col"> {{$ivyk->komanda_2}}</th>
+                                    <th scope="col"> {{$ivyk->koeficientas_2}}</th>
+                                    <th scope="col"> {{$ivyk->trukme}}</th>
+
+                                </tr>
+                            @endforeach
+                        </table>
+
+                    </div>
+                </div>
             </div>
             <div class="col-md-4">
                 <div class="card">
